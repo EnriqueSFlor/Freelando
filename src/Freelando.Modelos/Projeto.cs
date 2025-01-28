5e3da7e52327632b2ef81bfddb254f645684f986
enriq <enriq@ENRIQUE>
2025-01-27 21:37:47 -0300

    modificação

--- src/Freelando.Modelos/Projeto.cs
+++ src/Freelando.Modelos/Projeto.cs
@@ -11,6 +11,7 @@ public class Projeto
     public Guid Id { get; set; }
     public string? Titulo { get; set; }
 
+    public string? Titulo2 { get; set; }
     public  string? Descricao { get; set; }
     public StatusProjeto Status { get; set; }
 
